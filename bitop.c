e1e8b5a4b63586b227ad9c073cdcb84792c0c6e5
AllegroScherzando <j1597537@gmail.com>
2019-02-27 23:44:35 +0800

    Add Marco CHANGE
    *add an macro CHANGE
    *Fix some errors

--- bitop.c
+++ bitop.c
@@ -5,28 +5,32 @@
 #define CLEAR(number, n) number &= ~(1UL << n)
 #define TOGGLE(number, n) number ^= 1UL << n
 #define CHECK(number, n) (number >> n) & 1UL
+#define CHANGE(number, x, n) number ^= (-x ^ number) & (1UL << n);
 
 int main(void)
 {
     int num = 0x00000000;
     printf("%d\n", num);            // 0
 
-    SET(num, 0);                    // 0x00000001;
+    SET(num, 0);                    // 0...00000001;
     printf("%d\n", num);            // 1
-    
-    SET(num, 1);                    // 0x00000011;
+
+    SET(num, 1);                    // 0...00000011;
     printf("%d\n", num);            // 3
-    
-    CLEAR(num, 1);                  // 0x00000001;
+
+    CLEAR(num, 1);                  // 0...00000001;
     printf("%d\n", num);            // 1
-    
-    TOGGLE(num, 2);                 // 0x00000101;
+
+    TOGGLE(num, 2);                 // 0...00000101;
     printf("%d\n", num);            // 5
-    
-    TOGGLE(num, 2);                 // 0x00000001;
+
+    TOGGLE(num, 2);                 // 0...00000001;
     printf("%d\n", num);            // 1
     printf("%d\n", CHECK(num, 0));  // 0
     printf("%d\n", CHECK(num, 1));  // 1
+
+    CHANGE(num, 1, 4);              // 0...00010001;
+    printf("%d\n", num);            // 17;
 
     return 0;
 }
